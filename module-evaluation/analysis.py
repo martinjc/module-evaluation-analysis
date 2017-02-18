6046431ee295b05aee68272e10c40e28311d38b1
Martin Chorley <me@martinjc.com>
2017-02-18 14:14:58 +0000

    not sure where I was going with this

---
+++ module-evaluation/analysis.py
@@ -0,0 +1,32 @@
+LECTURER_COLUMNS = [
+    ': Was good at explaining things.',
+    ': Was available when needed.',
+    ': Made the subject interesting.'
+]
+
+MODULE_COLUMNS = [
+    'Module',
+    'I had a good idea of what to expect from this module.',
+    'The module met my expectations in terms of the knowledge I have gained.',
+    'I feel confident in communicating the knowledge I have gained on the module.',
+    'The module has helped my personal development by improving my employability skills (e.g. presentation skills, communication skills).',
+    'The range of the module’s resources (on Learning Central and/or in paper form) has effectively supported my learning.',
+    'Reading materials (books, journals, etc.) for this module were readily accessible.',
+    'The teaching rooms used for this module were suitable for the style of delivery.',
+    'I had a clear sense of what is required of me in the assessment/s for this module.',
+    'The criteria used to mark my work were made clear in advance.',
+    'Feedback on my marked work was provided within the specified timeframe.',
+    'Feedback has helped me to clarify things I did not understand.',
+    'The module has been well organised.',
+    'Any changes in the module or teaching were communicated effectively.',
+    'Overall, I am satisfied with the quality of this module.'
+]
+
+LIKERT = {
+    0.0: 'N/A',
+    1.0: 'Disagree Strongly',
+    2.0: 'Disagree',
+    3.0: 'Neither Agree nor Disagree',
+    4.0: 'Agree',
+    5.0: 'Agree Strongly'
+}
---
+++ module-evaluation/extract_data.py
@@ -0,0 +1,108 @@
+import os
+import pandas
+
+from analysis import MODULE_COLUMNS, LIKERT
+
+"""
+
+Takes a list of dataframes containing the results of module evaluation tasks
+and pulls out the results for each module as a set of csvs.
+
+Produces:
+
+module_count.csv
+        the number of responses for each module in the supplied dataframes
+
+module_percent_agreement_comparison.csv
+        the percentage agreement for each question for each module, along
+        with the average over all modules in the supplied dataframes
+
+<module>_percent_scores.csv
+        the percentage scores for each Likert value for each question
+        generated for each module in the supplied dataframes
+
+<module>_percent_agreement_comparison.csv
+        the percentage agreement for each question, along with the average
+        over all modules in the supplied dataframes, generated per module
+
+"""
+
+
+def combine_module_evaluation_data(dataframes):
+
+    # combine the data frames into one, dropping any NaNs
+    all_module_data_frames = []
+    for df in dataframes:
+        ad_f = df[MODULE_COLUMNS].dropna()
+        all_module_data_frames.append(ad_f)
+    all_module_data = pandas.concat(all_module_data_frames)
+
+    return all_module_data
+
+
+def convert_to_likert_and_combine(module_data):
+    # don't need 'Module' column
+    if module_data.get('Module'):
+        del module_data['Module']
+
+    module_data_counts = module_data.apply(lambda x: x.value_counts(normalize=True))
+    new_index = [LIKERT[value] for value in LIKERT.keys() if value in module_data_counts.index]
+    module_data_counts.index = new_index
+    module_data_counts.fillna(0, inplace=True)
+    module_data_counts = module_data_counts.reset_index().replace({'index': {'Disagree Strongly':'Disagree', 'Agree Strongly': 'Agree'}}).groupby('index', sort=False).sum()
+
+    return module_data_counts
+
+
+def get_module_count(all_module_data):
+
+    data_by_module = all_module_data.groupby('Module')
+    module_count = pandas.Series(index=data_by_module.groups.keys())
+
+    for module in data_by_module.groups:
+        module_data = data_by_module.get_group(module).dropna(axis=1, how='all')
+        module_count[module] = len(module_data.index)
+
+    return module_count
+
+
+def transpose_and_name_index(module_data, index_name):
+
+    module_data_T = module_data.T
+    module_data_T.index.name = index_name
+    return module_data_T
+
+
+def generate_module_mean_comparison(all_module_data):
+
+    module_comparison = pandas.DataFrame(index=MODULE_COLUMNS)
+    data_by_module = all_module_data.groupby('Module')
+    module_comparison['AllModules'] = all_module_data.mean()
+
+    for module in data_by_module.groups():
+        module_data = data_by_module.get_group(module)
+        del module_data['Module']
+        module_comparison[module] = module_data.mean()
+
+    return module_comparison
+
+
+def generate_module_percentage_scores(all_module_data):
+
+    module_comparison = pandas.DataFrame(index=[m for m in MODULE_COLUMNS if m != 'Module'])
+    data_by_module = all_module_data.groupby('Module')
+
+    all_module_data_counts = convert_to_likert_and_combine(all_module_data)
+    all_module_data_counts_T = transpose_and_name_index(all_module_data_counts, 'question')
+    module_comparison['AllModules'] = all_module_data_counts_T.ix['Agree']
+
+    for module in data_by_module.groups:
+
+            module_agreement_comparison = pandas.DataFrame(index=[m for m in MODULE_COLUMNS if m != 'Module'])
+
+            module_data = data_by_module.get_group(module).dropna(axis=1, how='all')
+
+            module_data_counts = convert_to_likert_and_combine(module_data)
+            module_data_counts_T = transpose_and_name_index(module_data_counts, 'question')
+
+            module_agreement_comparison[module] = module_data_T.ix['Agree']
