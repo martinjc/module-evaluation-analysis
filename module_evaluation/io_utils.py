5e5148b15472ffd98f200d9999d59c6155b5e720
Martin Chorley <me@martinjc.com>
2017-06-04 13:06:18 +0100

    extra filename type

--- module_evaluation/io_utils.py
+++ module_evaluation/io_utils.py
@@ -3,3 +3,6 @@ def construct_filename_identifier(identifier):
 
 def construct_filename_identifier_and_occurence(identifier, occurence):
     return '%s - (%s).csv' % (identifier, occurence)
+
+def construct_filename_lecturer_module_occurence(lecturer, module, occurence):
+    return construct_filename_identifier_and_occurence('%s - %s' % (lecturer, module), occurence)
