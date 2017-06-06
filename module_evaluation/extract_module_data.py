70656bc386acb91574a43ddf1e4c31c17b071501
Martin Chorley <me@martinjc.com>
2017-06-06 14:16:58 +0100

    added extraction of module specific column names

--- module_evaluation/extract_module_data.py
+++ module_evaluation/extract_module_data.py
@@ -29,17 +29,23 @@ def get_module_list(dataframes):
                 modules.append(m)
     return modules
 
+def get_module_columns(dataframes):
+
+    all_module_data = combine_module_evaluation_data(dataframes)
+    if 'Module' in all_module_data.columns:
+        del all_module_data['Module']
+    return all_module_data.columns
+
 def combine_module_evaluation_data(dataframes):
 
     # combine the data frames into one, dropping any NaNs
     all_module_data_frames = []
     for df in dataframes:
         # lecturer specific columns contain ':'
-        non_lecturer_columns = [c for c in df.columns if c.find(':') != -1]
+        non_lecturer_columns = [c for c in df.columns if c.find(':') == -1]
         ad_f = df[non_lecturer_columns].dropna()
         all_module_data_frames.append(ad_f)
-    all_module_data = pandas.concat(all_module_data_frames).dropna(axis=1, inplace=True)
-
+    all_module_data = pandas.concat(all_module_data_frames)
     return all_module_data
 
 
@@ -51,6 +57,7 @@ def get_module_and_occurence_data_with_lecturer(dataframes, modules, occurence):
         all_module_data_frames.append(df.loc[df['Module'].isin(['%s/%s' % (module, occurence) for module in modules])])
 
     all_module_data = pandas.concat(all_module_data_frames)
+
     return all_module_data
 
 
