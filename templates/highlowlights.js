6e602c73e2d321f75c909f7ce67deaece87f0b58
Martin Chorley <me@martinjc.com>
2017-06-10 15:04:53 +0100

    modularise highlowlights tables

--- templates/highlowlights.js
+++ templates/highlowlights.js
@@ -1,54 +1,22 @@
-(function(){
-
-    var highlights_table = d3.select('.highlights table tbody');
-    var lowlights_table = d3.select('.lowlights table tbody');
+function highlowTable() {
 
     var colourScale = d3.scaleLinear()
-        .domain([0,0.7,1])
+        .domain([0, 0.7, 1])
         .range(["hsla(0, 60%, 50%, 1)", "hsla(45, 70%, 60%, 1)", "hsla(90, 50%, 50%, 1)"]);
 
     var pformat = d3.format('.1%');
 
-    var module_data = d3.csvParse(d3.select('#data_agreement_comparison').html(), function(d){
-        return {
-            question: d.question,
-            module: +d.module,
-            AllModules: +d.AllModules
-        }
-    });
+    function table(selection) {
+        selection.each(function(data) {
+            table = d3.select(this);
+            data.forEach(function(d) {
+                table.append('tr')
+                    .style('color', colourScale(d.Agree))
+                    .html('<td>' + d.question + '</td><td>' + pformat(d.Agree) + '</td><td style="color: ' + colourScale(d.average) + ';">' + pformat(d.average) + '</td>')
+            });
 
-    module_data = module_data.filter(function(d){
-        return d.question !== "Overall, I am satisfied with the quality of this module.";
-    })
-
-    module_data.sort(function(a, b){
-        return b.module - a.module;
-    })
-
-    var lowlights = module_data.slice(module_data.length-3, module_data.length);
-    var highlights = module_data.slice(0, 3);
-
-    highlights_table.selectAll('tr')
-        .data(highlights)
-        .enter()
-        .append('tr')
-        .style('color', function(d){
-            return colourScale(d.module);
-        })
-        .html(function(d){
-            return '<td>' + d.question + '</td><td>' + pformat(d.module) + '</td><td style="color: ' + colourScale(d.AllModules) + ';">' + pformat(d.AllModules) + '</td>'
         });
+    }
 
-    lowlights_table.selectAll('tr')
-        .data(lowlights)
-        .enter()
-        .append('tr')
-        .style('color', function(d){
-            return colourScale(d.module);
-        })
-        .html(function(d){
-            return '<td>' + d.question + '</td><td>' + pformat(d.module) + '</td><td>' + pformat(d.AllModules) + '</td>'
-        });
-
-
-})();
+    return table;
+};
